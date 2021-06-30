ab07c499281f30b34e372ec3db754b85da2d652c
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-30 18:48:36 -0300

    refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { RepositoryList } from './components/RepositoryList';
+import { SearchBar } from './components/SearchBar';
 
 import { GlobalStyles } from './styles/global';
 
+export type RepositoryProps = {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+};
+
 function App(): JSX.Element {
+  const [repositories, setRepositories] = useState<RepositoryProps[]>([]);
+
   return (
     <>
       <GlobalStyles />
+      <SearchBar setRepositories={setRepositories} />
+      <RepositoryList repositories={repositories} />
     </>
   );
 }
